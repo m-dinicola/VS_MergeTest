c651147fd01526ef73b2982d0aa2f8cbc287c415
TxnUkeGrl <txnukegrl@gmail.com>
2021-05-20 19:20:09 -0400

    is this thing on?

--- VS_MergeTest/Program.cs
+++ VS_MergeTest/Program.cs
@@ -7,6 +7,7 @@ namespace VS_MergeTest
         static void Main(string[] args)
         {
             Console.WriteLine("I can't do that, Dave.");
+            Console.WriteLine("What's the deal with airline food, anyway?"); //Andrijka's contribution
         }
     }
 }
