25fe0cd6525de752039ba8b671b4ad2cbcb02711
mdinicola <the.m.dinicola@gmail.com>
2021-05-19 21:57:23 -0400

    commit 0

---
+++ VS_MergeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS_MergeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
